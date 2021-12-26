e7c9c73d6865db04479898e1d3c059d81661fd7f
Cube-Line <liuchen@qq.com>
2021-12-26 22:51:04 +0800

    同步进度

--- Core/Inc/ec01g_drv.h
+++ Core/Inc/ec01g_drv.h
@@ -14,8 +14,26 @@
 #include "stdint.h"
 #include "stm32f0xx_hal.h"
 
-// #define EC_POWER 
-void EC_Init(void);
+struct ec_module
+{
+	char *reset_module "AT+ECRST\n";							//复位EC-01G;
+	char *close_flight_mode "AT+CFUN=1\n";						//关闭飞行模式;
+	char *get_iccid "AT+ECICCID=1";								//查询SIM卡ICCID号;
+	char *attached_network "AT+CGATT=1";						//附着网络;
+	char *register_APNID "AT + CGDCONT = 1, \"IP\", \"CMNET\""; //注册APNID;
+	char *active_network "AT+CGACT=1";							//激活网络;
+	char *test_net_status "AT+ECPING=\"www.baidu.com\"";		// ping网络,测试网络状态;
+	char *connect_tcp_web_server "AT+SKTCREATE=1,1,6";			//使用TCP协议连接到服务器;
+	char *connect_udp_web_server "AT+SKTCREATE=1,2,17";			//使用UDP协议连接到服务器;
+	char *start_connect "AT+SKTCONNECT=";						//开始连接
+	char *act_gps "AT+GPS=1\n";									//开启GPS;
+	char *get_iemi "AT+CGSN=1\n";								//获取设备IEMI号;
+	char *get_singnal_strength "AT+CSQ\n";						//获取信号质量;
+};
+
+// #define EC_POWER
+void
+EC_Init(void);
 void EC_Uart_Send(uint8_t *data);
 void EC_Uart_Received(void);
 
--- Core/Src/ec01g_drv.c
+++ Core/Src/ec01g_drv.c
@@ -8,12 +8,40 @@
  *
  * */
 #include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
 #include "usart.h"
 #include "ec01g_drv.h"
 
-char uart_data[] = "AT+CGSN=0\n";
+// #define EC_RST
 
-void EC_Power_Control(uint8_t EC_PWR)
+char *EC_NB_init_data[] = {
+	"AT+ECRST\n",	//复位EC-01G;
+	"AT+CFUN=1\n",	//关闭飞行模式;
+	"AT+CGATT=1\n", //附着网络;
+};
+
+char *EC_create_mqtt[] = {
+	"AT + ECMTCFG = \"cloud\", 0, 3, 0\n",				//创建MQTT客户端
+	"AT +ECMTOPEN = 0,\"wx.ai-thinker.com\", 1883\n",	//打开客户端连接
+	"AT +ECMTCONN = 0,\"123\"，\"admin\",\"public\"\n", //创建连接
+};
+char *EC_recivemessage_mqtt[] = {
+	"AT+ECMTSUB=0,123,\"/IMEI/devPub\",0\n", //订阅数据;
+};
+
+char *EC_getlocation[] = {
+	"AT + LOCATION = 1\n", //获取位置信息;
+	"AT+GPSRD=1\n",		   //设置NMEA输出时间间隔;
+};
+char *EC_sendmessage_mqtt[] = {
+	"AT+ECMTPUB=0,1234,0,1,\"imei/DEVpUB\",\"{\"LONG\":value,\"qos\":value,\"temper\":value,\"hum\":value}\n", //上传数据;
+};
+
+
+
+void
+EC_Power_Control(uint8_t EC_PWR)
 {
 	if (0x01 == EC_PWR)
 		HAL_GPIO_WritePin(EC_PWR_GPIO_Port, EC_PWR_Pin, GPIO_PIN_SET);
@@ -24,5 +52,5 @@ void EC_Power_Control(uint8_t EC_PWR)
 void EC_Init(void)
 {
 	EC_Power_Control(0x01); //打开POWER_KEY(EC_RST);
-	HAL_UART_Transmit(&huart1, (uint8_t *)&uart_data, sizeof(uart_data), 1000);
+	HAL_UART_Transmit(&huart1, (uint8_t *)EC_NB_init_data, sizeof(&EC_NB_init_data), 1000);
 }
